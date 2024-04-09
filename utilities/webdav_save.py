0d1d1796ae23ae7f3c4e3932e0309d11bc03946a
Eric Kapalka <eric.kapalka@montana.edu>
2024-04-09 15:37:48 -0600

    Refactored check_file_exists to actually work (there were URL joining issues previously), as well as check the 'archive' sub-folder for the same file.

--- utilities/webdav_save.py
+++ utilities/webdav_save.py
@@ -1,22 +1,35 @@
+from urllib.parse import quote
 import requests
 
 
 def check_file_exists(server_url: str, path: str, username: str,
                       password: str, destination_name: str, **args) -> bool:
     """
-    Save a file to a WebDAV server
-    :param server_url: url of the server
-    :param path: path to the destination folder
-    :param username: username
-    :param password: password
-    :param destination_name: destination file name
-    :return: None
+    Check if a file or its archived version exists on a WebDAV server.
+    :param server_url: URL of the server.
+    :param path: Path to the destination folder.
+    :param username: Username for authentication.
+    :param password: Password for authentication.
+    :param destination_name: Name of the destination file.
+    :return: True if the file or its archived version exists, False otherwise.
     """
-    file_url = f"{server_url.rstrip('/')}/{path.rstrip('/')}/{destination_name}"
-    head_response = requests.head(file_url, auth=(username, password))
-    if head_response.status_code == 200:
-        # case when file already exists
-        return True
+    server_url = server_url.rstrip('/')
+    path = quote(path.strip('/'), safe='/')
+    destination_name = quote(destination_name, safe='/')
+    file_urls = [
+        f"{server_url}/{path}/{destination_name}",
+        f"{server_url}/{path}/archive/{destination_name}"
+    ]
+
+    with requests.Session() as session:
+        session.auth = (username, password)
+        for url in file_urls:
+            try:
+                if session.head(url).status_code == 200:
+                    return True
+            except requests.RequestException as e:
+                print(f"Error checking URL {url}: {e}")
+                continue
     return False
 
 
