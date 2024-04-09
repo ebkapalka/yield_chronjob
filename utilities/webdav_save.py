849165b5360f616974803580c7eb92ecf5747b46
Eric Kapalka <eric.kapalka@montana.edu>
2024-04-09 12:00:16 -0600

    changed file_name and file_content to destination_name and destination_content

--- utilities/webdav_save.py
+++ utilities/webdav_save.py
@@ -2,19 +2,19 @@ import requests
 
 
 def save_file_to_webdav(server_url: str, path: str, username: str, password: str,
-                        file_name: str, file_content: bytes) -> None:
+                        destination_name: str, destination_content: bytes) -> None:
     """
     Save a file to a WebDAV server
     :param server_url: url of the server
     :param path: path to the destination folder
     :param username: username
     :param password: password
-    :param file_name: destination file name
-    :param file_content: destination file content
+    :param destination_name: destination file name
+    :param destination_content: destination file content
     :return: None
     """
-    file_url = f"{server_url.rstrip('/')}/{path.rstrip('/')}/{file_name}"
-    response = requests.put(file_url, data=file_content, auth=(username, password))
+    file_url = f"{server_url.rstrip('/')}/{path.rstrip('/')}/{destination_name}"
+    response = requests.put(file_url, data=destination_content, auth=(username, password))
     if response.status_code in [200, 201]:
         print("File saved successfully.")
     else:
