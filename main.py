9bacda362b926a1e643227a65f625a3768137f08
Eric Kapalka <eric.kapalka@montana.edu>
2024-04-09 11:27:07 -0600

    Completely refactored to se YAML configuration

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
 from utilities.sftp_retrieve import (create_sftp_client, get_newest_yield,
-                                     download_file)
+                                     download_file_object)
 from utilities.credentials import retrieve_credentials
 
 import yaml
 
 
-if __name__ == '__main__':
-    remote_directory = "/users/ekapalka/jobsub/Tapeload/"
-    local_directory = "C:\\Users\\n18p538\\Documents"
-    service = "CopyYieldToKnox"
-    creds = {
-        "hostname": "jobsub-prod.msu.montana.edu",
-        "port": 22,
-        "username": "ekapalka",
-        "password": None,
+def download_file(config: dict) -> tuple[str, bytes]:
+    """
+    Download the newest file from the source
+    :param config: configuration dictionary
+    :return: tuple of file name and file content
+    """
+    sftp_config = {
+        "hostname": config["source"]["hostname"],
+        "port": config["source"]["port"],
+        "username": config["source"]["username"],
+        "password": retrieve_credentials(
+            config["source"]["service"],
+            config["source"]["username"])[1]
     }
-    # if need-be: delete_credentials(service, creds["username"])
-    _, creds["password"] = retrieve_credentials(
-        service, creds["username"])
-    sftp = create_sftp_client(**creds)
-    newest_file = get_newest_yield(sftp, remote_directory)
-    download_file(sftp, remote_directory,
-                  newest_file, local_directory)
+    sftp = create_sftp_client(**sftp_config)
+    newest_file_name = get_newest_yield(
+        sftp, config["source"]["sftp_directory"])
+    newest_file_content = download_file_object(
+        sftp, config["source"]["sftp_directory"], newest_file_name)
     sftp.close()
+    return newest_file_name, newest_file_content
+
+
+def save_file(config: dict, file_name: str, file_content: bytes) -> None:
+    """
+    Save a file to the destination
+    :param config: configuration dictionary
+    :param file_name: name of the file to save
+    :param file_content:
+    :return:
+    """
+    webdav_config = {
+        "server_url": config["destination"]["server_url"],
+        "path": config["destination"]["folder_path"],
+        "username": config["destination"]["username"],
+        "password": retrieve_credentials(
+            config["destination"]["service"],
+            config["destination"]["username"]),
+        "destination_name": file_name,
+        "destination_content": file_content
+    }
+
+
+if __name__ == '__main__':
+    with open("config.yml", "r") as file:
+        cfg = yaml.safe_load(file)
+    n, c = download_file(cfg)
+    print(n)
