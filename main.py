fb480806ee1d12242a251c563620d9d1e73089d4
Eric Kapalka <eric.kapalka@montana.edu>
2024-04-09 14:45:33 -0600

    Completely refactored for professionalism and scalability

--- main.py
+++ main.py
@@ -1,57 +1,83 @@
-from utilities.sftp_retrieve import (create_sftp_client, get_newest_yield,
-                                     download_file_object_pieces)
+from utilities.sftp_retrieve import (create_sftp_client, get_newest_file_name,
+                                     download_file_object)
+from utilities.webdav_save import save_file_to_webdav, check_file_exists
 from utilities.credentials import retrieve_credentials
-from utilities.webdav_save import save_file_to_webdav
 
 import yaml
 
 
-def download_file(config: dict) -> tuple[str, bytes]:
+class Config:
     """
-    Download the newest file from the source
-    :param config: configuration dictionary
-    :return: tuple of file name and file content
+    Class to parse the configuration file
     """
-    sftp_config = {
-        "hostname": config["source"]["hostname"],
-        "port": config["source"]["port"],
-        "username": config["source"]["username"],
-        "password": retrieve_credentials(
-            config["source"]["service"],
-            config["source"]["username"])[1]
-    }
-    sftp = create_sftp_client(**sftp_config)
-    newest_file_name = get_newest_yield(
-        sftp, config["source"]["sftp_directory"])
-    newest_file_content = download_file_object_pieces(
-        sftp, config["source"]["sftp_directory"], newest_file_name)
-    sftp.close()
-    return newest_file_name, newest_file_content
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.config = self._parse_config()
+        self.sftp_dir = self.config["source"]["sftp_directory"]
+
+    def _parse_config(self):
+        with open(self.file_path, "r") as file:
+            return yaml.safe_load(file)
+
+    def get_sftp_config(self) -> dict:
+        """
+        Get the SFTP configuration
+        :return: dictionary with SFTP configuration
+        """
+        creds = retrieve_credentials(
+            self.config["source"]["service"],
+            self.config["source"]["username"]
+        )
+        return {
+            "hostname": self.config["source"]["hostname"],
+            "port": self.config["source"]["port"],
+            "username": self.config["source"]["username"],
+            "password": creds[1]
+        }
+
+    def get_webdav_config(self):
+        """
+        Get the WebDAV configuration
+        :return: dictionary with WebDAV configuration
+        """
+        creds = retrieve_credentials(
+            self.config["destination"]["service"],
+            self.config["destination"]["username"]
+        )
+        return {
+            "server_url": self.config["destination"]["server_url"],
+            "path": self.config["destination"]["folder_path"],
+            "username": self.config["destination"]["username"],
+            "password": creds[1]
+        }
 
 
-def save_file(config: dict, file_name: str, file_content: bytes) -> None:
+def transfer_newest_file(sftp_config, webdav_config, sftp_directory):
     """
-    Save a file to the destination
-    :param config: configuration dictionary
-    :param file_name: name of the file to save
-    :param file_content: bytes of the file content
-    :return: None
+    Transfer the newest file from an SFTP server to a WebDAV server
+    :param sftp_config:
+    :param webdav_config:
+    :param sftp_directory:
+    :return:
     """
-    webdav_config = {
-        "server_url": config["destination"]["server_url"],
-        "path": config["destination"]["folder_path"],
-        "username": config["destination"]["username"],
-        "password": retrieve_credentials(
-            config["destination"]["service"],
-            config["destination"]["username"])[1],
-        "destination_name": file_name,
-        "destination_content": file_content
-    }
-    save_file_to_webdav(**webdav_config)
+    sftp_client = create_sftp_client(**sftp_config)
+    file_name = get_newest_file_name(sftp_client, sftp_directory)
+    print(f"Newest Yield file: {file_name}")
+    webdav_config_full = {**webdav_config, "destination_name": file_name}
+    if not check_file_exists(**webdav_config_full):
+        file_content = download_file_object(sftp_client, sftp_directory, file_name)
+        save_file_to_webdav(**webdav_config_full, destination_content=file_content)
+    else:
+        print(f"File {file_name} already exists")
+    sftp_client.close()
 
 
 if __name__ == '__main__':
-    with open("config.yml", "r") as file:
-        cfg = yaml.safe_load(file)
-    n, c = download_file(cfg)
-    save_file(cfg, n, c)
+    cfg_path = "config.yml"
+    cfg = Config(cfg_path)
+    sftp_cfg = cfg.get_sftp_config()
+    webdav_cfg = cfg.get_webdav_config()
+    sftp_dir = cfg.sftp_dir
+    transfer_newest_file(sftp_config=sftp_cfg,
+                         webdav_config=webdav_cfg,
+                         sftp_directory=sftp_dir)
