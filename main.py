62f74bb3c475862836e850f99d1d42dfe2d5622d
Eric Kapalka <eric.kapalka@montana.edu>
2024-04-09 13:38:47 -0600

    Cleaned up imports

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from utilities.sftp_retrieve import (create_sftp_client, get_newest_yield,
-                                     download_file_object, download_file_object_pieces)
+                                     download_file_object_pieces)
 from utilities.credentials import retrieve_credentials
+from utilities.webdav_save import save_file_to_webdav
 
 import yaml
-
-from utilities.webdav_save import save_file_to_webdav
 
 
 def download_file(config: dict) -> tuple[str, bytes]:
@@ -35,8 +34,8 @@ def save_file(config: dict, file_name: str, file_content: bytes) -> None:
     Save a file to the destination
     :param config: configuration dictionary
     :param file_name: name of the file to save
-    :param file_content:
-    :return:
+    :param file_content: bytes of the file content
+    :return: None
     """
     webdav_config = {
         "server_url": config["destination"]["server_url"],
